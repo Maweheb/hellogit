8ab8a167ec4e46280c7e09970722e325dc913222
Maweheb Belarbi <belarbimaweheb@gmail.com>
2018-02-01 11:20:53 +0100

    Keep one output

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -8,13 +8,7 @@ public class Helloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello World maww");
-		System.out.println("Maweheb");
-
-		System.out.println("Maweheb  branch fusion");
-		System.out.println("Maweheb untracked");
-		System.out.println("Maweheb branche");
-		System.out.println("ligne de code ajoutée sur Github");
-
+		
 	}
 
 }
